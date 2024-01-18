17a15ced8b48a406ebd4d06ddce58b55a7eaacbc
jiannei <longjian.huang@foxmail.com>
2024-01-18 16:21:44 +0800

    style: lint

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,9 +7,13 @@ const count = ref(0)
 </script>
 
 <template>
-    <div class="space-y-4">
-      <h1 class="text-2xl text-slate-500">{{ msg }}</h1>
+  <div class="space-y-4">
+    <h1 class="text-2xl text-slate-500">
+      {{ msg }}
+    </h1>
 
-      <button class="text-white bg-cyan-500 px-2 py-1 rounded-md" type="button" @click="count++">count is {{ count }}</button>
-    </div>
+    <button class="text-white bg-cyan-500 px-2 py-1 rounded-md" type="button" @click="count++">
+      count is {{ count }}
+    </button>
+  </div>
 </template>
