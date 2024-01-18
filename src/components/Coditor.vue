4cf506136912d6544a40031cd7b425ddafc5d7c2
jiannei <longjian.huang@foxmail.com>
2024-01-18 22:41:00 +0800

    wip: editor structure

--- src/components/Coditor.vue
+++ src/components/Coditor.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { MilkdownProvider } from '@milkdown/vue'
-import { Base64 } from 'js-base64'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useDark, useFileDialog } from '@vueuse/core'
-import { defaultValueCtx, editorViewOptionsCtx, rootCtx } from '@milkdown/core'
 import { shiki, shikiConfig } from '@s2nc/milkdown-plugin-shiki'
 import { commonmark } from '@milkdown/preset-commonmark'
 import { callCommand } from '@milkdown/utils'
 import { placeholderConfig, placeholder as placeholderPlugin } from '@s2nc/milkdown-plugin-placeholder'
-import { listener, listenerCtx } from '@milkdown/plugin-listener'
+import { listener } from '@milkdown/plugin-listener'
 import { remoteUpload, remoteUploadConfig, remoteUploader } from '@s2nc/milkdown-plugin-upload'
 import { uploadConfig } from '@milkdown/plugin-upload'
 import { Decoration } from '@milkdown/prose/view'
@@ -89,11 +87,7 @@ onChange((files) => {
 
 const isDark = useDark({ storageKey: 'theme' })
 
-const headingsRef = ref(props.headings)
-
-const editable = () => !props.readonly
-
-const plugins = [
+const plugins = ref([
   { plugin: commonmark },
   { plugin: listener }, // 只读模式不需要
   { plugin: clipboard }, // 只读模式不需要
@@ -101,12 +95,11 @@ const plugins = [
   { plugin: history }, // 只读模式不需要
   { plugin: indent },
   { plugin: emoji },
-  {
-    plugin: remoteUpload,
-    config: (ctx) => {
-      ctx.set(remoteUploadConfig.key, files => useImageUpload(props.upload.url, files))
-      ctx.set(uploadConfig.key, {
-        enableHtmlFileUploader: false,
+  { plugin: remoteUpload, config: (ctx) => {
+    ctx.set(remoteUploadConfig.key, files => useImageUpload(props.upload.url, files))
+    ctx.update(uploadConfig.key, (prev) => {
+      return {
+        ...prev,
         uploader: remoteUploader(ctx),
         uploadWidgetFactory: (pos, spec) => {
           const widgetDOM = document.createElement('span')
@@ -114,61 +107,24 @@ const plugins = [
 
           return Decoration.widget(pos, widgetDOM, spec)
         },
-      })
-    },
-  }, // 只读模式不需要
-  {
-    plugin: shiki,
-    config: ctx => ctx.set(shikiConfig.key, {
-      themes: { light: 'dracula', dark: 'dracula-soft' },
-      langs: ['bash', 'c', 'css', 'go', 'html', 'java', 'javascript', 'js', 'json', 'markdown', 'php', 'python', 'sql', 'sh', 'rust'],
-      dark: isDark.value,
-    }),
-  },
+      }
+    })
+  } }, // 只读模式不需要
+  { plugin: shiki, config: ctx => ctx.set(shikiConfig.key, {
+    themes: { light: 'dracula', dark: 'dracula-soft' },
+    langs: ['bash', 'c', 'css', 'go', 'html', 'java', 'javascript', 'js', 'json', 'markdown', 'php', 'python', 'sql', 'sh', 'rust'],
+    dark: isDark.value,
+  }) },
   { plugin: placeholderPlugin, config: ctx => ctx.set(placeholderConfig.key, props.placeholder) }, // 只读模式不需要
-]
+])
 
-function config(root) {
-  return {
-    plugins: plugins.map(item => item.plugin),
-    callback: (ctx) => {
-      // 初始化
-      ctx.set(rootCtx, root)
+const markdown = ref(props.content)
 
-      // 编辑模式
-      ctx.update(editorViewOptionsCtx, prev => ({
-        ...prev,
-        editable,
-      }))
-
-      // 主题
-      ctx.update(editorViewOptionsCtx, (prev) => {
-        return {
-          ...prev,
-          attributes: {
-            class: `${props.height !== 'full' ? `min-h-[${props.height}]` : ''} max-w-none prose prose-slate dark:prose-invert outline-none`,
-          },
-        }
-      })
-
-      // 默认内容
-      ctx.set(defaultValueCtx, Base64.decode(props.content))
-
-      // 监听内容更新
-      ctx.get(listenerCtx).markdownUpdated((ctx, markdown) => {
-        // if (cache)
-        // cacheStorage.value = content
-
-        emit('update:content', Base64.encode(markdown))
-      })
-      plugins.map(item => item.config).filter(item => item).forEach(item => item(ctx))
-    },
-  }
-}
+watch(markdown, value => emit('update:content', value))
 </script>
 
 <template>
   <MilkdownProvider>
-    <Editor ref="editor" :config="config" />
+    <Editor ref="editor" v-model:content="markdown" :readonly="readonly" :plugins="plugins" :height="height" />
   </MilkdownProvider>
 </template>
--- src/components/MilkdownEditor.vue
+++ src/components/MilkdownEditor.vue
@@ -1,18 +1,56 @@
 <script setup>
 import { Milkdown, useEditor } from '@milkdown/vue'
-import { Editor } from '@milkdown/core'
+import { Editor, defaultValueCtx, editorViewOptionsCtx, rootCtx } from '@milkdown/core'
+import { listenerCtx } from '@milkdown/plugin-listener'
 
-const { config } = defineProps({
-  config: {
-    type: Function,
-    required: true,
+const { height, content, plugins, readonly } = defineProps({
+  plugins: {
+    type: Array,
+    default: () => [],
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
+  height: { // todo 待移除
+    type: String,
+    default: 'full',
+  },
+  content: {
+    type: String,
+    default: '',
   },
 })
 
-const { get } = useEditor((root) => {
-  const { callback, plugins } = config(root)
+const emit = defineEmits(['update:content'])
 
-  return Editor.make().config(callback).use(plugins)
+const { loading, get } = useEditor((root) => {
+  return Editor.make().config((ctx) => {
+    ctx.set(rootCtx, root)
+
+    ctx.update(editorViewOptionsCtx, prev => ({
+      ...prev,
+      editable: () => !readonly,
+    }))
+
+    ctx.update(editorViewOptionsCtx, (prev) => {
+      return {
+        ...prev,
+        attributes: {
+          // todo 移除 height
+          class: `${height !== 'full' ? `min-h-[${height}]` : ''} max-w-none prose prose-slate dark:prose-invert outline-none`,
+        },
+      }
+    })
+
+    ctx.set(defaultValueCtx, content)
+
+    ctx.get(listenerCtx).markdownUpdated((ctx, markdown) => {
+      emit('update:content', markdown)
+    })
+
+    plugins.map(item => item.config).filter(item => item).forEach(item => item(ctx))
+  }).use(plugins.map(item => item.plugin))
 })
 
 defineExpose({ get })
