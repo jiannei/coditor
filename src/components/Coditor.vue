4d70423a3ab7bd69235cb2726fd409cd83f3cc46
jiannei <longjian.huang@foxmail.com>
2024-01-24 11:23:58 +0800

    feat: coditor

---
+++ src/components/Coditor.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { MilkdownProvider } from '@milkdown/vue'
+import { ref } from 'vue'
+import MilkdownEditor from './MilkdownEditor.vue'
+
+defineProps({
+  plugins: {
+    type: Array,
+    default: () => [],
+  },
+})
+
+const editorRef = ref()
+
+function get() {
+  return editorRef.value.get()
+}
+
+defineExpose({ get })
+</script>
+
+<template>
+  <MilkdownProvider>
+    <MilkdownEditor ref="editorRef" :plugins="plugins" />
+  </MilkdownProvider>
+</template>
