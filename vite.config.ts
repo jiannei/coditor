579ef7606d94279e021688897369fcc9c14ea7fb
jiannei <longjian.huang@foxmail.com>
2024-01-25 14:20:14 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -5,25 +5,8 @@ import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/coditor/',
   plugins: [vue(), UnoCSS()],
-  build: {
-    lib: {
-      entry: 'src/index.js',
-      formats: ['es', 'cjs', 'umd'],
-      name: 'coditor',
-      fileName: format => `coditor.${format}.js`,
-    },
-    rollupOptions: {
-      // https://rollupjs.org/configuration-options/
-      external: ['vue'],
-      output: {
-        exports: 'named', // https://rollupjs.org/configuration-options/#output-exports
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
